a0ee1fe97af93dee81e41c45729bd4082317b69c
Tabatha Zeitke <tzeitke@newrelic.com>
2022-01-11 18:37:45 -0800

    help

--- src/commands/Information/poof.js
+++ src/commands/Information/poof.js
@@ -10,81 +10,81 @@ module.exports = {
     .setDescription("Summon the summoner!"),
   async execute(interaction) {
     console.log("/poof command entered");
-    const url =
-      "https://discord.com/api/guilds/690636760698323065/scheduled-events";
-    // cron.schedule("*/10 * * * * *", async () => {
-    const events = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: `Bot ${process.env.TOKEN}`,
-      },
-    }).then((response) => response.json());
-
-    var upcomingEvent = events[0];
-
-    var upcomingDate = new Date(events[0].scheduled_start_time);
-    const now = new Date();
-
-    for (each of events) {
-      var tempDate = new Date(each.scheduled_start_time);
-
-      // check that the event hasn't started
-      if (now.getTime() < tempDate.getTime()) {
-        // time math to find the closest one
-        if (tempDate.getTime() < upcomingDate.getTime()) {
-          upcomingDate = tempDate;
-          upcomingEvent = each;
-        }
-      }
-    }
-
-    const month = upcomingDate.toLocaleString("en-US", {
-      month: "long",
-    });
-    const day = upcomingDate.toLocaleString("en-US", {
-      day: "numeric",
-    });
-    const weekday = upcomingDate.toLocaleString("en-US", {
-      weekday: "long",
-    });
-    const time = upcomingDate.toLocaleString("en-US", {
-      timeStyle: "short",
-    });
-
-    const formatDate = `${weekday}, ${month} ${day} at ${time}.`;
-
-    // cron.schedule("*/10 * * * * *", async () => {
-    // const getChannel = await fetch(
-    //   "https://discord.com/api//channels/690636761243844662",
-    //   {
+    //   const url =
+    //     "https://discord.com/api/guilds/690636760698323065/scheduled-events";
+    //   // cron.schedule("*/10 * * * * *", async () => {
+    //   const events = await fetch(url, {
     //     method: "GET",
     //     headers: {
-    //       Authorization:
-    //         `Bot ${process.env.TOKEN}`,
+    //       Authorization: `Bot ${process.env.TOKEN}`,
     //     },
+    //   }).then((response) => response.json());
+
+    //   var upcomingEvent = events[0];
+
+    //   var upcomingDate = new Date(events[0].scheduled_start_time);
+    //   const now = new Date();
+
+    //   for (each of events) {
+    //     var tempDate = new Date(each.scheduled_start_time);
+
+    //     // check that the event hasn't started
+    //     if (now.getTime() < tempDate.getTime()) {
+    //       // time math to find the closest one
+    //       if (tempDate.getTime() < upcomingDate.getTime()) {
+    //         upcomingDate = tempDate;
+    //         upcomingEvent = each;
+    //       }
+    //     }
     //   }
-    // ).then((response) => response.json());
-    // var formatLocation = "";
 
-    // await console.log(getChannel.name);
-    const exampleEmbed = new MessageEmbed()
-      .setTitle("Divination determines...")
-      .addFields(
-        {
-          name: "The next session is",
-          value: formatDate,
-        },
-        {
-          name: upcomingEvent.name,
-          value: upcomingEvent.description,
-        }
-      );
-    // .addField(`This events takes place ${formatLocation}`, "\u200b", true);
-    // await client.channels.cache
-    //   .get("927723006795583569")
-    //   .send({ embeds: [exampleEmbed] });
+    //   const month = upcomingDate.toLocaleString("en-US", {
+    //     month: "long",
+    //   });
+    //   const day = upcomingDate.toLocaleString("en-US", {
+    //     day: "numeric",
+    //   });
+    //   const weekday = upcomingDate.toLocaleString("en-US", {
+    //     weekday: "long",
+    //   });
+    //   const time = upcomingDate.toLocaleString("en-US", {
+    //     timeStyle: "short",
+    //   });
 
-    // await interaction.reply(`The next session is ${formatDate}`);
-    await interaction.reply({ embeds: [exampleEmbed] });
+    //   const formatDate = `${weekday}, ${month} ${day} at ${time}.`;
+
+    //   // cron.schedule("*/10 * * * * *", async () => {
+    //   // const getChannel = await fetch(
+    //   //   "https://discord.com/api//channels/690636761243844662",
+    //   //   {
+    //   //     method: "GET",
+    //   //     headers: {
+    //   //       Authorization:
+    //   //         `Bot ${process.env.TOKEN}`,
+    //   //     },
+    //   //   }
+    //   // ).then((response) => response.json());
+    //   // var formatLocation = "";
+
+    //   // await console.log(getChannel.name);
+    //   const exampleEmbed = new MessageEmbed()
+    //     .setTitle("Divination determines...")
+    //     .addFields(
+    //       {
+    //         name: "The next session is",
+    //         value: formatDate,
+    //       },
+    //       {
+    //         name: upcomingEvent.name,
+    //         value: upcomingEvent.description,
+    //       }
+    //     );
+    //   // .addField(`This events takes place ${formatLocation}`, "\u200b", true);
+    //   // await client.channels.cache
+    //   //   .get("927723006795583569")
+    //   //   .send({ embeds: [exampleEmbed] });
+
+    //   // await interaction.reply(`The next session is ${formatDate}`);
+    //   await interaction.reply({ embeds: [exampleEmbed] });
   },
 };
